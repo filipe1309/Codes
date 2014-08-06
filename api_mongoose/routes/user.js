d6bab9481de1967c45cdecef854667e56f295d16
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-08-06 00:43:44 -0300

    api_mongoose: Add insert function
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- api_mongoose/routes/user.js
+++ api_mongoose/routes/user.js
@@ -6,3 +6,12 @@ exports.list = function(req, res){
   //res.send('respond with a resource');
   UserModel.list(req, res);
 };
+
+
+exports.get = function(req, res) {
+	UserModel.get(req, res);
+};
+
+exports.create = function(req, res) {
+	UserModel.create(req, res);
+}
