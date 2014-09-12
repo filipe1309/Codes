244acf93bcbebd2cf588455f5f0e81fd0e379100
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-09-12 17:51:22 -0300

    android: Add app that implements 3 ways of ransfer data between activities, with single, intent and the class Application
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

---
+++ android/DataBetweenActivities_ISA/src/com/example/testet4/MySuperApplication.java
@@ -0,0 +1,9 @@
+package com.example.testet4;
+
+import android.app.Application;
+
+public class MySuperApplication extends Application{
+	
+	public String SomeSetting = "Dados com classe Application, classe MySuperApplication <--> MainActivity(ou qualquer outra acitvity)";
+
+}
