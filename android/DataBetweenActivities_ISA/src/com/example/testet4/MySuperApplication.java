7305d87546e28026361721cd5aa403b9c3ee9252
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-10-21 22:51:01 -0200

    android: Remove DataBetweenActivities_ISA because its not compatible with gradle and have app.copatv7 as dependency
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- android/DataBetweenActivities_ISA/src/com/example/testet4/MySuperApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.testet4;
-
-import android.app.Application;
-
-public class MySuperApplication extends Application{
-	
-	public String SomeSetting = "Dados com classe Application, classe MySuperApplication <--> MainActivity(ou qualquer outra acitvity)";
-
-}
