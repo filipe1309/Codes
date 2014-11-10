13ff63adafa46f262198fb330326a44c31ec6acd
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-11-09 23:05:26 -0200

    projects: Add  alterantive mean math race project
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

---
+++ projects/mean-math-race/public/javascripts/controllers.js
@@ -0,0 +1,59 @@
+angular.module('sonApp.controllers', ['sonApp.services'])
+	.controller('UsersCtrl',
+		['$scope', 'UsersSrv', '$routeParams', '$location',
+			function($scope, UsersSrv, $routeParams, $location) {
+				$scope.nome = "|Var do angular - controller.js|";
+			 	$scope.valid = false;
+
+				$scope.load = function() {
+					$scope.registros = UsersSrv.query();
+				}
+
+				$scope.get = function() {
+					$scope.item = UsersSrv.get({id: $routeParams.id});
+				}
+
+				$scope.edit = function(item) {
+					delete item._id; // Pois o mongo não aceita sobrescrever _id
+					$scope.result = UsersSrv.update(
+						{id: $routeParams.id},
+						item,
+						function(data, status, headers, config) {
+							$location.path("/");
+						}
+					);
+				}
+
+				$scope.add = function(item) {
+					$scope.result = UsersSrv.save(
+						{},
+						item,
+						function(data, status, headers, config) {
+							$location.path("/");
+						}
+					);
+				}
+
+				$scope.delete = function(id) {
+					if(confirm("Deseja Realmente excluir esse usuário?")) {
+						$scope.result = UsersSrv.remove(
+							{id: id},
+							{},
+							function(data, status, headers, config) {
+								$location.path("/");
+							}
+						);	
+					}
+				}
+
+				$scope.verifyName = function(item) {
+					if(item.name.length > 2) {
+					 	$scope.valid = true;
+					 } else {
+					 	$scope.valid = false;
+					 }					
+				}
+				//$scope.load();
+			}
+		]
+	);
