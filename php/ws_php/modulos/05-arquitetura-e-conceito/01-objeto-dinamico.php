fc655b6b878054877b04e8d0d4fdcc246a6700f9
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2015-08-07 00:32:07 -0300

    php: Create structure for the Dynamic Object lesson
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

---
+++ php/ws_php/modulos/05-arquitetura-e-conceito/01-objeto-dinamico.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>WS PHP - Objeto Dinamico</title>
+    </head>
+    <body>
+        <?php
+        require './inc/Config.inc.php';
+        echo '<pre>';
+       
+      
+        ?>
+    </body>
+</html>
---
+++ php/ws_php/modulos/05-arquitetura-e-conceito/class/ObjetoDinamico.class.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * ObjetoDinamico.class [ TIPO ]
+ * Descricao
+ * @copyright (c) year, Filipe Leuch Bonfim UPINSIDE
+ */
+class ObjetoDinamico {
+
+    
+}
