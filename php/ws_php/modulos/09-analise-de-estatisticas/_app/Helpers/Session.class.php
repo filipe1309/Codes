f6e07bda328d5d1fffa78038897a35346a297edf
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2015-09-09 22:51:07 -0300

    php: Add lessons of Statistics management for users, visits and updates
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- php/ws_php/modulos/09-analise-de-estatisticas/_app/Helpers/Session.class.php
+++ php/ws_php/modulos/09-analise-de-estatisticas/_app/Helpers/Session.class.php
@@ -27,13 +27,15 @@ class Session {
             $this->setTraffic();
             $this->setSession();
         else:
-            $this->setSession();
-
+            //$this->setSession();
+            //$this->setTraffic();
+            $this->trafficUpdate();
+            $this->sessionUpdate();
         endif;
 
         $this->date = null;
     }
-    
+
     // Inicia a sessão do usuário
     private function setSession() {
         $_SESSION['useronline'] = [
@@ -42,21 +44,42 @@ class Session {
             'online_endviews' => date('Y-m-d H:i:s', strtotime("+{$this->cache}minutes")),
             'online_ip' => filter_input(INPUT_SERVER, 'REMOTE_ADDR', FILTER_VALIDATE_IP),
             'online_url' => filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_DEFAULT),
-            'online_agent' => filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_DEFAULT)      
+            'online_agent' => filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_DEFAULT)
         ];
     }
 
+    // Atualiza sessão do usuario
+    private function sessionUpdate() {
+        $_SESSION['useronline']['online_endviews'] = date('Y-m-d H:i:s', strtotime("+{$this->cache}minutes"));
+        $_SESSION['useronline']['online_url'] = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_DEFAULT);
+    }
 
     // Verifica e insere o tráfego na tabela
     private function setTraffic() {
         $this->getTraffic();
-        if(!$this->traffic):
+        if (!$this->traffic):
             $arrSiteViews = ['siteviews_date' => $this->date, 'siteviews_users' => 1, 'siteviews_views' => 1, 'siteviews_pages' => 1];
             $createSiteViews = new Create;
-            $createSiteViews ->exeCreate('ws_siteviews', $arrSiteViews);
+            $createSiteViews->exeCreate('ws_siteviews', $arrSiteViews);
         else:
-            
+            if (!$this->getCookie()):
+                $arrSiteViews = ['siteviews_users' => $this->traffic['siteviews_users'] + 1, 'siteviews_views' => $this->traffic['siteviews_views'] + 1, 'siteviews_pages' => $this->traffic['siteviews_pages'] + 1];
+            else:
+                $arrSiteViews = ['siteviews_views' => $this->traffic['siteviews_views'] + 1, 'siteviews_pages' => $this->traffic['siteviews_pages'] + 1];
+            endif;
+
+            $updateSiteViews = new Update;
+            $updateSiteViews->exeUpdate('ws_siteviews', $arrSiteViews, 'WHERE siteviews_date = :date', "date={$this->date}");
+
         endif;
+    }
+
+    // Verifica e atualiza os pageviews
+    private function trafficUpdate() {
+        $this->getTraffic();
+        $arrSiteViews = ['siteviews_pages' => $this->traffic['siteviews_pages'] + 1];
+        $updatePageViews = new Update;
+        $updatePageViews->exeUpdate('ws_siteviews', $arrSiteViews, 'WHERE siteviews_date = :date', "date={$this->date}");
     }
 
     // Obtem dados da tabela [ HELPER TRAFFIC ]
@@ -68,16 +91,17 @@ class Session {
             $this->traffic = $readSiteViews->getResult()[0];
         endif;
     }
-    
+
     // Verifica, cria e atualiza o cookie do usuário [ HELPER TRAFFIC ]
     private function getCookie() {
         $cookie = filter_input(INPUT_COOKIE, 'useronline', FILTER_DEFAULT);
-        if(!$cookie):
+        setcookie('useronline', base64_encode('upinside'), time() + 86400); // 1 dia
+
+        if (!$cookie):
             return false;
         else:
             return true;
         endif;
-        
-        setcookie('useronline', base64_encode('upinside'), time() + 86400); // 1 dia
     }
+
 }
