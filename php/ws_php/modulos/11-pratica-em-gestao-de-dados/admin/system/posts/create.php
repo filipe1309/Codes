fff3b8d6029b1156b9485fb00bc950df3b310b55
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2015-10-26 20:44:23 -0200

    php: Add lessons of Struturing the Post Controller
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- php/ws_php/modulos/11-pratica-em-gestao-de-dados/admin/system/posts/create.php
+++ php/ws_php/modulos/11-pratica-em-gestao-de-dados/admin/system/posts/create.php
@@ -6,10 +6,19 @@
             <h1>Criar Post:</h1>
         </header>
 
+
         <?php
-        EchoMsg("Erro ao cadastrar:", "Existem campos ogrigatórios sem preencher.", ALERT);
-        EchoMsg("Erro ao cadastrar:", "A capa deve ser JPG, PNG ou GIF com até 2MB!.", ALERT);
-        EchoMsg("Sucesso:", "Seu post foi cadastrado com sucesso. <a target=\"_blank\" href=\"../artigo/titulo\">Ver Post</a>", ACCEPT);
+//        EchoMsg("Erro ao cadastrar:", "Existem campos ogrigatórios sem preencher.", ALERT);
+//        EchoMsg("Erro ao cadastrar:", "A capa deve ser JPG, PNG ou GIF com até 2MB!.", ALERT);
+//        EchoMsg("Sucesso:", "Seu post foi cadastrado com sucesso. <a target=\"_blank\" href=\"../artigo/titulo\">Ver Post</a>", ACCEPT);
+
+        $post = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+        if (isset($post) && $post['SendPostForm']):
+
+            echo '<pre>';
+            var_dump($post);
+            echo '</pre>';
+        endif;
         ?>
 
 
@@ -17,37 +26,78 @@
 
             <label class="label">
                 <span class="field">Enviar Capa:</span>
-                <input type="file" name="cover" />
+                <input type="file" name="post_cover" />
             </label>
 
             <label class="label">
                 <span class="field">Titulo:</span>
-                <input type="text" name="post_title" />
+                <input type="text" name="post_title" value="<?php if (isset($post['post_title'])) echo $post['post_title']; ?>" />
             </label>
 
             <label class="label">
                 <span class="field">Conteúdo:</span>
-                <textarea class="js_editor" name="post_content" rows="10"></textarea>
+                <textarea class="js_editor" name="post_content" rows="10"><?php if (isset($post['post_content'])) echo htmlspecialchars($post['post_content']); ?></textarea>
             </label>
 
             <div class="label_line">
 
                 <label class="label_small">
                     <span class="field">Data:</span>
-                    <input type="text" class="formDate center" name="post_title" value="<?= date('d/m/Y H:i:s'); ?>" />
+                    <input type="text" class="formDate center" name="post_date" value="<?php
+                    if (isset($post['post_date'])): echo $post['post_date'];
+                    else: echo date('d/m/Y H:i:s');
+                    endif;
+                    ?>" />
                 </label>
 
                 <label class="label_small">
                     <span class="field">Categoria:</span>
                     <select name="post_category">
                         <option value=""> Selecione a categoria: </option>
+                        <?php
+                        $readSes = new Read;
+                        $readSes->exeRead('ws_categories', 'WHERE category_parent IS NULL ORDER BY category_title ASC');
+                        if ($readSes->getRowCount() >= 1):
+                            foreach ($readSes->getResult() as $ses):
+                                echo "<option disabled=\"disabled\" value=\"\"> {$ses['category_title']}</option>";
+                                $readCat = new Read;
+
+                                $readCat->exeRead('ws_categories', 'WHERE category_parent = :parent ORDER BY category_title ASC', "parent={$ses['category_id']}");
+                                if ($readCat->getRowCount() >= 1):
+                                    foreach ($readCat->getResult() as $cat):
+                                        echo "<option ";
+                                        if ($post['post_category'] == $cat['category_id']):
+                                            echo "selected=\"selected\" ";
+                                        endif;
+                                        echo "value=\"{$cat['category_id']}\"> &raquo;&raquo; {$cat['category_title']} </option>";
+                                    endforeach;
+                                endif;
+                            endforeach;
+                        endif;
+                        ?>
                     </select>
                 </label>
 
                 <label class="label_small">
                     <span class="field">Author:</span>
-                    <select name="post_category">
-                        <option value=""> <?= "{$_SESSION['userlogin']['user_name']} {$_SESSION['userlogin']['user_lastname']}"; ?> </option>
+                    <select name="post_author">
+                        <option value="<?= $_SESSION['userlogin']['user_id']; ?>"> <?= "{$_SESSION['userlogin']['user_name']} {$_SESSION['userlogin']['user_lastname']}"; ?> </option>
+                        <?php
+                        $readAut = new Read;
+                        $readAut->exeRead('ws_users', 'WHERE user_id != :id AND user_level >= :level ORDER BY user_name ASC', ":id={$_SESSION['userlogin']['user_id']}&level=2");
+                        var_dump($readAut);
+                        if ($readAut->getRowCount() >= 1):
+                            foreach ($readAut->getResult() as $aut):
+                                echo "<option ";
+                                if ($post['post_author'] == $aut['user_id']):
+                                    echo "selected=\"selected\" ";
+                                endif;
+                                echo "value=\"{$aut['user_id']}\"> {$aut['user_name']} {$aut['user_lastname']} </option>";
+                            endforeach;
+                        else:
+                            echo "<option value=\"3123\"> zica</option>";
+                        endif;
+                        ?>
                     </select>
                 </label>
 
@@ -60,19 +110,19 @@
                     <input type="file" multiple name="gallery_covers" />
                 </label>
 
-                <ul class="gallery">
-                    <?php for ($i = 1; $i <= 10; $i++): ?>
+                <ul class="gallery" style="display: none">
+<?php for ($i = 1; $i <= 10; $i++): ?>
                         <li<?php if ($i % 5 == 0) echo ' class="right"'; ?>>
                             <div class="img thumb_small"></div>
                             <a class="del" href="#delete">Deletar</a>                    
                         </li>
-                    <?php endfor; ?>
+<?php endfor; ?>
                 </ul>                
             </div>
 
 
             <input type="submit" class="btn blue" value="Cadastrar" name="SendPostForm" />
-            <input type="submit" class="btn green" value="Cadastrar & Publicar" name="SendPostFormPublish" />
+            <input type="submit" class="btn green" value="Cadastrar & Publicar" name="SendPostForm" />
 
         </form>
 
