384edfc955961865dc79cdf4bd2d61a23c00203a
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2015-11-05 20:42:18 -0200

    php: Add lessons of Listing and managing posts
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- php/ws_php/modulos/11-pratica-em-gestao-de-dados/admin/system/posts/index.php
+++ php/ws_php/modulos/11-pratica-em-gestao-de-dados/admin/system/posts/index.php
@@ -4,35 +4,64 @@
 
         <h1>Posts:</h1>
 
-        <?php for ($i = 1; $i <= 10; $i++): ?>
-            <article<?php if($i%2==0) echo ' class="right"';?>>
+        <?php
+        $posti = 0;
+        $getPage = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+        $pager = new Pager('painel.php?exe=posts/index&page=');
+        $pager->exePager($getPage, 2);
 
-                <div class="img thumb_small"></div>
-                <h1><a target="_blank" href="../artigo/uri-do-artigo" title="Ver Post">Tchau iPhone: Galaxy S3 supera o 4S e é o celular mais vendido do mundo</a></h1>
-                <ul class="info post_actions">
-                    <li><strong>Data:</strong> <?= date('d/m/Y H:i'); ?>Hs</li>
-                    <li><a class="act_view" target="_blank" href="painel.php?exe=posts/post&id=ID_DO_POST" title="Ver no site">Ver no site</a></li>
-                    <li><a class="act_edit" href="painel.php?exe=posts/post&id=ID_DO_POST" title="Editar">Editar</a></li>
-                    <!--<li><a class="act_inative" href="painel.php?exe=posts/post&id=ID_DO_POST" title="Ativar">Ativar</a></li>-->
-                    <li><a class="act_ative" href="painel.php?exe=posts/post&id=ID_DO_POST" title="Inativar">Ativar</a></li>
-                    <li><a class="act_delete" href="painel.php?exe=posts/post&id=ID_DO_POST" title="Excluir">Deletar</a></li>
-                </ul>
+        $readPosts = new Read;
+        $readPosts->exeRead('ws_posts', 'ORDER BY post_status ASC, post_date DESC LIMIT :limit OFFSET :offset', "limit={$pager->getLimit()}&offset={$pager->getOffset()}");
+        if ($readPosts->getResult()):
+            foreach ($readPosts->getResult() as $post):
+                $posti++;
+                extract($post);
+                $status = (!$post_status ? 'style="background:#fffed8"' : '');
+                ?>
+                <article<?php if ($posti % 2 == 0) echo ' class="right"'; ?> <?= $status; ?>>
 
-            </article>
-        <?php endfor; ?>
+                    <div class="img thumb_small">
+                        <?= Check::image('../uploads/' . $post_cover, $post_title, 120, 70); ?>
+                    </div>
+                    <h1><a target="_blank" href="../artigo/<?= $post_name; ?>" title="Ver Post"><?= $post_title ?></a></h1>
+                    <ul class="info post_actions">
+                        <li><strong>Data:</strong> <?= date('d/m/Y H:i', strtotime($post_date)); ?>Hs</li>
+                        <li><a class="act_view" target="_blank" href="../artigo/<?= $post_name; ?>" title="Ver no site">Ver no site</a></li>
+                        <li><a class="act_edit" href="painel.php?exe=posts/update&postid=<?= $post_id; ?>" title="Editar">Editar</a></li>
+
+                        <?php if ($post_status): ?>
+                            <li><a class="act_inative" href="painel.php?exe=posts/post&post=<?= $post_id; ?>&action=active" title="Ativar">Ativar</a></li>
+                        <?php else: ?>
+                            <li><a class="act_ative" href="painel.php?exe=posts/post&post=<?= $post_id; ?>&action=inative" title="Inativar">Inativar</a></li>
+                        <?php endif; ?>
+                        <li><a class="act_delete" href="painel.php?exe=posts/index&delete=<?= $post_id; ?>" title="Excluir">Deletar</a></li>
+                    </ul>
+
+                </article>
+                <?php
+            endforeach;
+        else:
+            $pager->returnPage();
+            wsErro("Desculpe, ainda não existem posts cadastrados", WS_INFOR);
+        endif;
+        ?>
 
         <div class="clear"></div>
     </section>
-    
-    <div class="paginator">
-        <a href="#">Primeira Página</a>
-        <a href="#">1</a>
-        <a href="#">2</a>
-        <span class="atv">3</span>
-        <a href="#">4</a>
-        <a href="#">5</a> 
-        <a href="#">Última Página</a>
-    </div>
+
+    <?php
+    $pager->exePager('ws_posts');
+    echo $pager->getPaginator();
+    ?>
+    <!--    <div class="paginator">
+            <a href="#">Primeira Página</a>
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <span class="atv">3</span>
+            <a href="#">4</a>
+            <a href="#">5</a> 
+            <a href="#">Última Página</a>
+        </div>-->
 
     <div class="clear"></div>
 </div> <!-- content home -->
